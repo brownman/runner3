a68c2749cca384bb93d66c733bff08e86141d4a6
brownman <brownman556@gmail.com>
2014-12-04 12:56:04 +0200

    test: mv code to Ex: .sh

--- circle.sh
+++ circle.sh
@@ -1,1 +1,9 @@
 #
+ docker run brownman/runner3 sudo -u gitlab_ci_runner -H 'env' > /tmp/env.txt
+ cp /tmp/env.txt $CIRCLE_ARTIFACTS/env.txt
+ 
+  docker run brownman/runner3 sudo -u gitlab_ci_runner -H 'ps -aux' > /tmp/ps.txt
+ cp /tmp/ps.txt $CIRCLE_ARTIFACTS/ps.txt
+ 
+   docker run brownman/runner3 sudo -u gitlab_ci_runner -H 'netstat -ntlp' > /tmp/netstat.txt
+ cp /tmp/netstat.txt $CIRCLE_ARTIFACTS/netstat.txt
